d9bce80499d7cfe8e976f65703e8569e88905791
S1mpleKnight <become.adult77@gmail.com>
2021-04-01 20:32:03 +0300

    delete option done

--- OOP_3/src/main/java/by/bsuir/oop/third/controller/TablesController.java
+++ OOP_3/src/main/java/by/bsuir/oop/third/controller/TablesController.java
@@ -4,6 +4,7 @@ import by.bsuir.oop.third.furniture.Table;
 import by.bsuir.oop.third.info.Info;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -18,7 +19,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class TablesController {
-
+    DTO dto;
     @FXML
     private Button addButton;
 
@@ -41,12 +42,27 @@ public class TablesController {
     void initialize() {
         areaColumn.setCellValueFactory(area -> area.getValue().area.asString());
         weightColumn.setCellValueFactory(weight -> weight.getValue().weight.asString());
-        ObservableList<DTO> tables = FXCollections.observableArrayList(convert(Info.getInfo().getTables().getList()));
+        ArrayList<DTO> dtos = convert(Info.getInfo().getTables().getList());
+        ObservableList<DTO> tables = FXCollections.observableArrayList(dtos);
         tableTables.setItems(tables);
+
+        TableView.TableViewSelectionModel<DTO> selected = tableTables.getSelectionModel();
+        selected.selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            dto = newValue;
+        });
+
+        deleteButton.setOnAction(e -> {
+            if (dto != null) {
+                int index = dtos.indexOf(dto);
+                dtos.remove(index);
+                Info.getInfo().getTables().getList().remove(index);
+                tableTables.setItems(FXCollections.observableArrayList(dtos));
+            }
+        });
     }
 
-    private static List<DTO> convert(List<Table> tables){
-        List<DTO> dtos = new LinkedList<>();
+    private static ArrayList<DTO> convert(List<Table> tables){
+        ArrayList<DTO> dtos = new ArrayList<>();
         for (Table table : tables){
             dtos.add(new DTO(table.getArea(), table.getWeight()));
         }
